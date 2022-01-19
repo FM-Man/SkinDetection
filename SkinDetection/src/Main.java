4599b65fdc1b44f04c9edac096d76846ce21df88
fahim-morshed-1102 <bsse1102@iit.du.ac.bd>
2022-01-20 00:49:54 +0600

    implemented threshold
    white = face

--- SkinDetection/src/Main.java
+++ SkinDetection/src/Main.java
@@ -2,7 +2,10 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     public static int[][][] colourArrayNonSkin = new int[256][256][256];
@@ -17,7 +20,7 @@ public class Main {
         File[] Maskfolder = new File("Mask").listFiles();
         File[] RealFolder = new File("Real").listFiles();
 
-        for(int imageindex =0 ; imageindex < 200; imageindex++){
+        for(int imageindex =0 ; imageindex < 500; imageindex++){
             BufferedImage imgMask = ImageIO.read(Maskfolder[imageindex]);
             BufferedImage img = ImageIO.read(RealFolder[imageindex]);
 
@@ -37,23 +40,52 @@ public class Main {
                     }
                 }
             }
+            System.out.println(imageindex + "reading done");
+        }
 
-            for(int i=0; i<256; i++){
-                for(int j=0; j<256; j++){
-                    for(int k=0; k<256; k++){
-                        probabilitySkin[i][j][k] = colourArraySkin[i][j][k]/skin;
-                        probabilityNonSkin[i][j][k] = colourArrayNonSkin[i][j][k]/nonSkin;
-                    }
+        System.out.println("reading done");
+
+        FileWriter skinProbFile = new FileWriter("skin.txt",true);
+        FileWriter nonSkinProbFile = new FileWriter("nonskin.txt");
+        for(int i=0; i<256; i++){
+            for(int j=0; j<256; j++){
+                for(int k=0; k<256; k++){
+                    probabilitySkin[i][j][k] = colourArraySkin[i][j][k]/skin;
+                    probabilityNonSkin[i][j][k] = colourArrayNonSkin[i][j][k]/nonSkin;
+                    skinProbFile.write(probabilitySkin[i][j][k]+"\n");
+                    nonSkinProbFile.write(probabilityNonSkin[i][j][k]+"\n");
+                    System.out.println(i + " " + j + " " +k);
                 }
             }
         }
+        skinProbFile.close();
+        nonSkinProbFile.close();
     }
 
     public static void main(String[] args) throws IOException {
-        readImage();
-        System.out.println("reading done");
+        //readImage();
+        //already trained with 500 images
+        
+        Scanner scS = new Scanner(new File("skin.txt"));
+        Scanner scNS = new Scanner(new File("nonskin.txt"));
 
-        BufferedImage SampleImage = ImageIO.read(new File("rb.jpg"));
+        for(int i=0; i<16777216; i++){
+            int x = i;
+            int c = x%256;
+            x/=256;
+            int b = x%256;
+            x/=256;
+            int a = x;
+
+            double ss = scS.nextDouble();
+            double sns = scNS.nextDouble();
+            probabilityNonSkin[a][b][c] = sns;
+            probabilitySkin[a][b][c] = ss;
+
+            //System.out.println(a+" "+b+" "+c);
+        }
+
+        BufferedImage SampleImage = ImageIO.read(new File("in4.jpg"));
         BufferedImage outputImage = new BufferedImage(SampleImage.getWidth(), SampleImage.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
 
         int width = SampleImage.getWidth();
@@ -65,16 +97,15 @@ public class Main {
             int alpha = (imageInPixels[i] & 0xFF000000) >> 24;
             int red = (imageInPixels[i] & 0x00FF0000) >> 16;
             int green = (imageInPixels[i] & 0x0000FF00) >> 8;
-            int blue = (imageInPixels[i] & 0x000000FF) >> 0;
+            int blue = (imageInPixels[i] & 0x000000FF);
 
-            // Make any change to the colors.
-            if (  probabilityNonSkin[red][green][blue] > probabilitySkin[red][green][blue] ){
+            // not face=black
+            if ( probabilitySkin[red][green][blue] / probabilityNonSkin[red][green][blue] > 0.35  ){
                 red = 255;
                 green = 250;
                 blue = 250;
 
             } else {
-
                 red =10 ;
                 green = 10;
                 blue =10;
